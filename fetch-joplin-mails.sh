166c407b4f0db6af88f58b956a243fd155236bee
Manfred Geiler <manfred.geiler@irian.at>
2019-01-13 04:19:16 +0100

    switched back to always syncing

--- fetch-joplin-mails.sh
+++ fetch-joplin-mails.sh
@@ -20,8 +20,8 @@ NEW_MAIL=0
 
 fetchMails "$POP3_USER" "$POP3_PW" "$MAILDIR"
 
-NEW_MAIL_FILES=`find "$MAILDIR/new" -type f -print0 | sort -z`
-while read -d $'\0' M; do
+find "$MAILDIR/new" -type f -print0 | sort -z | while read -d $'\0' M
+do
     echo "-------------------"
     echo "Process $M"
     let NEW_MAIL=1
@@ -31,12 +31,10 @@ while read -d $'\0' M; do
     else
         echo "Error: Mail could not be added - leaving in inbox"
     fi
-done <<< "$NEW_MAIL_FILES"
+done
 
-if [[ ${NEW_MAIL} -eq 1 ]] ; then
-    echo "-------------------"
-    echo "Start Joplin Sync"
-    joplin sync
-fi
+echo "-------------------"
+echo "Start Joplin Sync"
+joplin sync
 
 echo "End: `date`"
