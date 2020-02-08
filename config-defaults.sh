06f729214a16e78ad0ac3e4da9710cd9faea253d
Manfred Geiler <manfred.geiler@irian.at>
2020-02-08 17:55:02 +0100

    added some comments to config-defaults.sh regarding getmail -d flag

--- config-defaults.sh
+++ config-defaults.sh
@@ -6,9 +6,15 @@ POP3_USER="unknown"
 POP3_PW="unknown"
 POP3_HOST="pop.gmail.com"
 POP3_PORT=995
+
 GETMAIL_OPTS=--ssl
+
+# As soon as your setup is tested and working you may add the -d flag
+# to delete fetched mails from your provider inbox:
+#GETMAIL_OPTS=--ssl -d
 
 DEFAULT_TITLE_PREFIX="New Note"
 DEFAULT_NOTEBOOK="Inbox"
 
+# Shall new notebooks (given via @-syntax) be created automatically?
 AUTO_CREATE_NOTEBOOK=false
