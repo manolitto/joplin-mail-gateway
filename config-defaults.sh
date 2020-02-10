ef7ef9d038e449b84394910112db9185b50ed190
Manfred Geiler <manfred.geiler@irian.at>
2020-02-10 23:58:48 +0100

    added some comments to config-sample.sh regarding getmail -d flag

--- config-defaults.sh
+++ config-defaults.sh
@@ -9,12 +9,7 @@ POP3_PORT=995
 
 GETMAIL_OPTS=--ssl
 
-# As soon as your setup is tested and working you may add the -d flag
-# to delete fetched mails from your provider inbox:
-#GETMAIL_OPTS=--ssl -d
-
 DEFAULT_TITLE_PREFIX="New Note"
 DEFAULT_NOTEBOOK="Inbox"
 
-# Shall new notebooks (given via @-syntax) be created automatically?
 AUTO_CREATE_NOTEBOOK=false
--- config-sample.sh
+++ config-sample.sh
@@ -6,8 +6,16 @@ POP3_USER="your-email-user@your-provider"
 POP3_PW="your-super-secret-pop3-pw"
 POP3_HOST="pop.gmail.com"
 POP3_PORT=995
-#GETMAIL_OPTS=--ssl
+
+# Note: By default emails do get not deleted from your inbox which results in duplicate notes on every run.
+# This is for the safety of your data and protects you from losing emails due to a bad setup.
+# However, as soon as your setup is tested and working you may add the --delete flag to the getmail options.
+# Alternative: if your email provider allows it (Gmail does) you may also set your inbox options to
+# "archive mails when POP3 fetched".
+#GETMAIL_OPTS=--ssl --delete
 
 #DEFAULT_TITLE_PREFIX="Neue Notiz"
 #DEFAULT_NOTEBOOK="Import"
+
+#AUTO_CREATE_NOTEBOOK=false
 
